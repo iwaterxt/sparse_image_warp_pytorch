44f66d1203785b8a177d3807154ff26222eaa832
iwaterxt <iwaterxt@gmail.com>
2019-09-02 13:10:45 +0800

    update

--- utils/sparse_image_warp.py
+++ utils/sparse_image_warp.py
@@ -119,7 +119,7 @@ def solve_interpolation(train_points, train_values, order, regularization_weight
     rhs = torch.cat((f, rhs_zeros), 1)  # [b, n + d + 1, k]
 
     # Then, solve the linear system and unpack the results.
-    X, LU = torch.gesv(rhs, lhs)
+    X, LU = torch.solve(rhs, lhs)
     w = X[:, :n, :]
     v = X[:, n:, :]
 
